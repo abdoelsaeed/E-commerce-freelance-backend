18992a4fdd1c10a4bb0b7c18d912a9ec7cb1a643
abdoelsaeed <abdoelsaeed290@icloud.com>
2025-10-21 21:38:56 +0300

    update mongoose

--- DB/connectionDB.js
+++ DB/connectionDB.js
@@ -1,10 +1,32 @@
+// DB/connectionDB.js
 const mongoose = require("mongoose");
 
-const db =
+const MONGO_URI =
   process.env.DB ||
   "mongodb+srv://abdoelsaeed2:12345@cluster000.h7jdjme.mongodb.net/e-commerce-freelancer?retryWrites=true&w=majority&appName=Cluster000";
-mongoose
-  .connect(db)
-  .then(() => console.log("DB Connection Successfully"))
-  .catch((err) => console.error("DB connection error: ", err));
-  module.exports = db;
+
+if (!MONGO_URI) throw new Error("MONGO_URI is not defined");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+module.exports = connectDB();
