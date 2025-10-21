7f47f42efb195321db8738d45a534b648e0ae72b
abdoelsaeed <abdoelsaeed290@icloud.com>
2025-10-21 22:31:15 +0300

    update error.controller

--- DB/connectionDB.js
+++ DB/connectionDB.js
@@ -5,6 +5,11 @@ const db =
   "mongodb+srv://abdoelsaeed2:12345@cluster000.h7jdjme.mongodb.net/e-commerce-freelancer?retryWrites=true&w=majority&appName=Cluster000";
 
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    maxPoolSize: 10,
+  })
   .then(() => console.log("DB Connection Successfully"))
   .catch((err) => console.error("DB connection error: ", err));
