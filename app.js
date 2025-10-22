01b00084091a070325ca0ec96f718d4aa9663f04
abdoelsaeed <abdoelsaeed290@icloud.com>
2025-10-22 05:25:29 +0300

    update json

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "https://e-commerce-freelance-frontend-gpxky2wr6-abdoelsaeeds-projects.vercel.app/",
+      "https://e-commerce-freelance-frontend-gpxky2wr6-abdoelsaeeds-projects.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
